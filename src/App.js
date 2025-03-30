6059f97232daee6da1660d01f92dcc0b97e7041a
Tanmay Muthiyan <tanmaymuthiyan008@gmail.com>
2025-03-30 14:47:52 +0530

    headerandbanner

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import './App.css';
 import Demo2 from './Components/Megha/Demo2';
 import TourSection from './Components/Megha/TourSection';
 import Demo from './Components/Kartik/Demo';
-
+import './App.css';
+import Header from './Components/Tanmay/Header';
+import Banner from './Components/Tanmay/Banner';
 import CustomersReviews from './Components/Megha/CustomersReviews';
 import Footer from './Components/Footer/Footer';
 import ContactForm from './Components/Megha/ContactForm';
-//import demo2 from './Components/Megha/demo2'
-//function App() {
-  //return (
     
-    // App.js
-
 const App = () => {
   return (
     <div className="App">
-      
+       <Header/>
+       <Banner/>
       <Demo2 />
       <TourSection />
       < CustomersReviews />
@@ -24,18 +22,13 @@ const App = () => {
     
       <Footer/>
       <ContactForm/>
+    
+     
     </div>
   );
 }
 
 export default App;
 
-  //   <>
-  //   <Demo2/>
-  //   </>
-    
-  // );
-// }
 
-//export default App;
 
